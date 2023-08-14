514e73210c66aad010e7fc72a7996ce301d541c0
GYLIARDE FARIAS ARAUJO <gyliarde@live.com>
2023-08-13 21:15:18 -0300

    init project

---
+++ src/main/kotlin/Ch01/Arrays_and_Strings/Q1_01_Is_Unique/IsUnique.kt
@@ -0,0 +1,23 @@
+package Ch01.Arrays_and_Strings.Q1_01_Is_Unique
+
+/* Determine a function if string has all a unique character */
+
+fun isUniqueChars(str: String): Boolean {
+    if (str.length > 128) return false
+
+    val charSet = BooleanArray(128)
+    for (element in str) {
+        val strVal = element.code
+        if (charSet[strVal]) return false
+        charSet[strVal] = true
+    }
+    return true
+}
+
+fun main() {
+    val words = arrayOf("abcde", "hello", "apple", "kite", "padle")
+
+    for (word in words) {
+        println(" $word  : ${isUniqueChars(word)} ")
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+
+    val ageMap = HashMap<String, Int>()
+    ageMap["Alice"] = 10
+
+    println(ageMap["Alice"])
+
+
+
+}
